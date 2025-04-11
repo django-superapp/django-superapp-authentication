adc4fa434d437bb02e79c715e58283aeb0ebca0c
ajb78 <a.jordan1@gmx.de>
2025-04-11 15:07:23 +0200

    feat: remove all references to easywindows

--- migrations/0002_initial.py
+++ migrations/0002_initial.py
@@ -12,24 +12,13 @@ class Migration(migrations.Migration):
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
         ('authentication', '0001_initial'),
-        ('easywindow', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='user',
-            name='default_organization',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='easywindow.organization', verbose_name='default organization'),
-        ),
-        migrations.AddField(
-            model_name='user',
             name='groups',
             field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups'),
-        ),
-        migrations.AddField(
-            model_name='user',
-            name='location',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='users', to='easywindow.location'),
         ),
         migrations.AddField(
             model_name='user',
